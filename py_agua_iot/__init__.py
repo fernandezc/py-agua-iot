ced595e88943e13a60a69df9f69378e97691491c
Christian Fernandez <christian.fernandez@ensicaen.fr>
2022-12-05 19:31:09 +0100

    doc link i+ fix  `temp_air_get`

--- py_agua_iot/__init__.py
+++ py_agua_iot/__init__.py
@@ -580,7 +580,8 @@ class Device(object):
 
     @property
     def air_temperature(self):
-        return float(self.__get_information_item('temp_air2_get'))
+        return float(self.__get_information_item('temp_air_get')) # 'temp_air2_get' 
+                                                                  # not working for me
 
     @property
     def set_air_temperature(self):
